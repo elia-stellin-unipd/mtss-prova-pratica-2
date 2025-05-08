de5236067597e301e5a69cb634d244e32da56c1f
mscompar <mattia.scomparin.1@studenti.unipd.it>
2025-05-08 15:21:59 +0200

    Implementazione e test dei numeri 7-10; close #4

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -5,22 +5,40 @@
 package it.unipd.mtss;
 
 public class IntegerToRoman {
+
     public static String convert(int number) {
-        switch(number) {
-            case 1:
-                return "I";
-            case 2:
-                return "II";
-            case 3:
-                return "III";
-            case 4:
-                return "IV";
-            case 5:
-                return "V";
-            case 6:
-                return "VI";
-            default:
-                return null;
+        //Legacy Code 
+        //switch(number) {
+        //     case 1:
+        //         return "I";
+        //     case 2:
+        //         return "II";
+        //     case 3:
+        //         return "III";
+        //     case 4:
+        //         return "IV";
+        //     case 5:
+        //         return "V";
+        //     case 6:
+        //         return "VI";
+        //     default:
+        //         return null;
+        // }
+        String[] chars = {"X", "IX", "V", "IV", "I"};
+        int[] values = {10, 9, 5, 4, 1};
+
+        if(number > values[0] || number < values[values.length-1]) {
+            return null;
         }
+
+        StringBuilder res = new StringBuilder();
+
+        for (int index = 0; index < values.length; index++) {
+            while(number - values[index] >= 0){
+                number -= values[index];
+                res.append(chars[index]);
+            }
+        }
+        return res.toString();
     }
 }
--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -11,12 +11,17 @@ import org.junit.jupiter.api.Test;
 public class IntegerToRomanTest {
 
     @Test
+    public void TestNumeri_OutOfBounds() {
+        assertEquals(IntegerToRoman.convert(0), null);
+        assertEquals(IntegerToRoman.convert(11), null);
+    }
+
+    @Test
     public void TestNumeri_Uno_Tre() {
         IntegerToRoman converter = new IntegerToRoman();
         assertEquals(converter.convert(1), "I");
         assertEquals(converter.convert(2), "II");
         assertEquals(converter.convert(3), "III");
-        assertEquals(converter.convert(0), null);
     }
 
     @Test
@@ -25,5 +30,13 @@ public class IntegerToRomanTest {
         assertEquals(converter.convert(4), "IV");
         assertEquals(converter.convert(5), "V");
         assertEquals(converter.convert(6), "VI");
+    }
+
+    @Test
+    public void TestNumeri_7_10() {
+        assertEquals(IntegerToRoman.convert(7), "VII");
+        assertEquals(IntegerToRoman.convert(8), "VIII");
+        assertEquals(IntegerToRoman.convert(9), "IX");
+        assertEquals(IntegerToRoman.convert(10), "X");
     }
 }
