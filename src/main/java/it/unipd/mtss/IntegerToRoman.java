f78d57471161a80210e14643bde1d3169bdeddb6
mscompar <mattia.scomparin.1@studenti.unipd.it>
2025-05-08 11:22:43 +0200

    Funzionalità conversione numeri da 1 a 3 + test. Close #2

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -2,3 +2,19 @@
 // Elia Ernesto Stellin 2101084
 // Mattia Scomparin 2110992
 ////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+public class IntegerToRoman {
+    public static String convert(int number) {
+        switch(number) {
+            case 1:
+                return "I";
+            case 2:
+                return "II";
+            case 3:
+                return "III";
+            default:
+                return null;
+        }
+    }
+}
--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -2,3 +2,20 @@
 // Elia Ernesto Stellin 2101084
 // Mattia Scomparin 2110992
 ////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class IntegerToRomanTest {
+
+    @Test
+    public void TestNumeri_Uno_Tre() {
+        IntegerToRoman converter = new IntegerToRoman();
+        assertEquals(converter.convert(1), "I");
+        assertEquals(converter.convert(2), "II");
+        assertEquals(converter.convert(3), "III");
+        assertEquals(converter.convert(0), null);
+    }
+}
